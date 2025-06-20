4cfe9e8feffce0a462e8d35b09d8ccf969606cee
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-06-19 19:46:59 -0600

    dumpster card working

--- intermountain-dumpsters/next.config.ts
+++ intermountain-dumpsters/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'acsxwvvvlfajjizqwcia.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/dumpster-images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
