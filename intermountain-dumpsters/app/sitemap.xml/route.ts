1a452e75e4dc2cab7d60b02d041b07aa5493e4f8
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-06-30 10:04:55 -0600

    test

--- intermountain-dumpsters/app/sitemap.xml/route.ts
+++ intermountain-dumpsters/app/sitemap.xml/route.ts
@@ -1,31 +1,9 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const baseUrl = "https://www.intermountaindumpsters.com";
-  const urls = [
-    "",
-    "/book",
-    "/service-areas",
-    "/contact",
-    "/privacy-policy"
-  ];
-
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.map(
-      (path) => `
-  <url>
-    <loc>${baseUrl}${path}</loc>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-    <lastmod>${new Date().toISOString()}</lastmod>
-  </url>
-`).join('')}
-</urlset>`;
-
-  return new NextResponse(xml, {
+  return new NextResponse('HELLO SITEMAP', {
     headers: {
-      'Content-Type': 'application/xml',
+      'Content-Type': 'text/plain',
     },
   });
 } 
