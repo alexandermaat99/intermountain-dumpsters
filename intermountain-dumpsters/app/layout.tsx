874224e742fbeda97120db082edec79403b76b1b
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-06-30 10:59:14 -0600

    Update favicon and manifest icons for improved platform support

--- intermountain-dumpsters/app/layout.tsx
+++ intermountain-dumpsters/app/layout.tsx
@@ -102,11 +102,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {/* Favicons for Google, Android, and browsers */}
+        {/* Favicons for all platforms - use green logo */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/GrenLogoIcon192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/GrenLogoIcon512.png" />
-        <link rel="apple-touch-icon" href="/GrenLogoIcon192.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         {/* Google Tag Manager */}
         {process.env.NEXT_PUBLIC_GTM_ID && (
           <Script
