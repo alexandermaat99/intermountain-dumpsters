e002193e6a8f25be94b8ed62a9038d09b50135cd
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-06-30 09:11:38 -0600

    hardcode

--- intermountain-dumpsters/app/sitemap.ts
+++ intermountain-dumpsters/app/sitemap.ts
@@ -1,9 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "https://www.intermountaindumpsters.com"
+  const baseUrl = "https://www.intermountaindumpsters.com";
 
   return [
     {
